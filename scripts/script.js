64ec1101ac3c56de1ea9e53caa23d160c0d4ef47
Fabiana Monteiro Menocci da Silva <fabi_mms_05@hotmail.com>
2023-05-14 23:34:52 -0300

    Commit da entrega final do projeto

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,42 @@
-function fazGet(url) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.send();
-    return xhr.responseText;
+const apiUrl = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id';
+
+// Chamar a função para cada herói na sequencia do html
+getHeroData('346'); // ID do herói
+getHeroData('332'); 
+getHeroData('107');
+getHeroData('659');
+getHeroData('149');
+getHeroData('620');
+getHeroData('720');
+getHeroData('644');
+getHeroData('70');
+getHeroData('38');
+getHeroData('265');
+getHeroData('194');
+
+// Chamar a função para cada herói
+function getHeroData(heroId) {
+   fetch(`${apiUrl}/${heroId}.json`)
+      .then(response => response.json())
+      .then(data => {
+         const heroElement = document.querySelector(`.hero[data-hero-id="${heroId}"]`);
+         const heroNameElement = heroElement.querySelector('.hero-name');
+         const fullNameElement = heroElement.querySelector('.full-name');
+         const nicknameElement = heroElement.querySelector('.nickname');
+         const occupationElement = heroElement.querySelector('.occupation');
+
+         // Extrair os dados da resposta da API
+         const heroName = data.name;
+         const fullName = data.biography['fullName'];
+         const nickname = data.biography['aliases'][0];
+         const occupation = data.work.occupation;
+
+         // Atualizar os elementos HTML com os dados extraídos
+         heroNameElement.textContent = heroName;
+         fullNameElement.textContent = fullName;
+         nicknameElement.textContent = nickname;
+         occupationElement.textContent = occupation;
+
+      })
+      .catch(error => console.log(error));
 }
-
-function criaLinha(usuario) {
-
-}
-
-function main() {
-   console.log(fazGet("https://akabab.github.io/superhero-api/api"))
-}
-
-main();
-
-
-
-// var xhr = new XMLHttpRequest();
-
-// xhr.onreadystatechange = function() {
-//     if (xhr.readyState === 4 && xhr.status === 200) {
-//         var response = JSON.parse(xhr.responseText);
-
-//         console.log(response);
-
-//     }
-// }
-
-// xhr.open('GET', 'https://akabab.github.io/superhero-api/api', true);
-// xhr.send();
-
-
-
-
-
-
-
-
-
-
-
-//var bandeira = resultado[0].flags;
-
-        //const img = document.querySelector('.bandeira');
-        //img.src = bandeira.png;
-        //img.alt = bandeira.alt;
-
-        //document.appendChild(img);
